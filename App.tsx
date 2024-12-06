9278364dbfeaa31df9c2951a23fd8baae8831c11
SaidLeon04 <1722110300@utectulancingo.edu.mx>
2024-12-06 12:16:32 -0600

    Init VisionCamera

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Camera, useCameraDevices } from "react-native-vision-camera";
 
-export default function App() {
+const App = () => {
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const devices = useCameraDevices();
+  const device = devices.find((device) => device.position === "front");
+
+  useEffect(() => {
+    (async () => {
+      const permissionResult = await Camera.requestCameraPermission();
+      setCameraPermission(permissionResult === 'granted');
+    })();
+  }, []);
+  
+  
+
+  if (!device || !cameraPermission) {
+    return <Text>Solicitando acceso a la cámara frontal...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {device && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+        />
+      )}
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Capturar</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { flex: 1 },
+  button: {
+    position: "absolute",
+    bottom: 20,
+    alignSelf: "center",
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
   },
+  buttonText: { color: "white", fontWeight: "bold" },
 });
+
+export default App;
